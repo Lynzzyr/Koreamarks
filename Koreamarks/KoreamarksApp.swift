4a1cbd44a6e74e47c3029ba3e82fcc45aebc89cb
Lynzzyr <66968682+Lynzzyr@users.noreply.github.com>
2024-02-10 15:16:06 -0500

    Initial Commit

---
+++ Koreamarks/KoreamarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  KoreamarksApp.swift
+//  Koreamarks
+//
+//  Created by Brandon Namgoong on 2024-02-10.
+//
+
+import SwiftUI
+
+@main
+struct KoreamarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
